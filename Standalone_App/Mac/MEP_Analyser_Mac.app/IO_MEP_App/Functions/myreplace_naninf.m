f065f5a201c6e650d3cad96443b98a742f3cffe1
Ke Ma <37124808+BIOMAKE@users.noreply.github.com>
2023-12-15 16:32:44 +0000

    Delete Standalone_App/Mac/MEP_Analyser_Mac.app/IO_MEP_App directory

--- Standalone_App/Mac/MEP_Analyser_Mac.app/IO_MEP_App/Functions/myreplace_naninf.m
+++
@@ -1,6 +0,0 @@
-function returnme = myreplace_naninf(inputvar)
-%   ersetzt NaN und Inf gegen sqrt(realmax) oder dergleichen,
-%   bei fit sollte Minimierer dann auch nicht dorthin konvergieren
-
-returnme = inputvar;
-returnme(not(isfinite(returnme))) = 1e90;   %sqrt(realmax);
